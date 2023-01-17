618e958a4f5b60d5e6114b59944ab1739ca784af
MatthewWood-Github <matthewwood2002@gmail.com>
2023-01-17 17:51:25 +0000

    Changes

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+/*
 class unit {
     constructor(name, price, range, power, type)
+} */
+
+currentUnit = null;
+shopOptions = [];
+
+function populateOptions() {
+    shopOptions = document.getElementById("shop-options").children;
 }
+
+function updateBorders() {
+    for (elem of shopOptions) {
+        elem.style.borderColor = "black";
+    }
+
+    currentUnit.style.borderColor = "yellow";
+}
+
+function buttonAnimation(id) {
+    elem = document.getElementById(id)
+    elem.classList.remove("button-animation");
+    void elem.offsetWidth;
+    elem.classList.add("button-animation");
+    currentUnit = elem;
+    updateBorders();
+}
+
+populateOptions();
